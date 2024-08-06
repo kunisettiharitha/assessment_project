91c272e492580d314c26e3be7821c2a757094eeb
kunisettiharitha <haritha.kunisetti@gmail.com>
2024-08-06 18:30:39 +0530

    created search component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React, { useState } from "react";
-import Data from './db.json';
+import React from "react";
+import Search from './Search';
 const App = () => {
-  const[search,setSearch] = useState(" ");
+  
   return (
     <div>
     <center>
-      <h4>Enter your city:</h4> <br />
-      <input type="text" 
-      value={search}
-      onChange={(e)=>setSearch(e.target.value)} 
-      />  <br />
-      {Data.filter(city=>city.name.toLowerCase().includes(search.toLowerCase())).map(city => 
-      {
-        return <div key={city.key} style={{"border":"1px solid black" , "padding": "10px" , "margin": "10px" , "maxWidth": "50%"}}> 
-             {city.name}
-            </div>
-      }
-      )}
+     <Search />
     </center>
      
     </div>
